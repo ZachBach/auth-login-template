14b0f2d5e040e3a32bf77e2e5f0ffc323e52ca7a
Zach Auerbach <auernachzach@gmail.com>
2020-12-26 12:23:37 -0800

    added a private route so user has to login to see the dashboard. As well as created update password and password reset using firebase

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,9 @@ import { AuthProvider } from '../contexts/AuthContext';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Dashboard from './Dashboard';
 import Login from './Login';
+import PrivateRoute from './PrivateRoute';
+import ForgotPassword from './ForgotPassword';
+import UpdateProfile from './UpdateProfile';
 
 function App() {
   return (
@@ -15,9 +18,13 @@ function App() {
         <Router>
           <AuthProvider>
             <Switch>
-              <Route exact path='/' component={Dashboard}></Route>
+              <PrivateRoute exact path='/' component={Dashboard}></PrivateRoute>
+              <PrivateRoute
+                path='/update-profile'
+                component={UpdateProfile}></PrivateRoute>
               <Route path='/signup' component={Signup}></Route>
               <Route path='/login' component={Login}></Route>
+              <Route path='/forgot-password' component={ForgotPassword} />
             </Switch>
           </AuthProvider>
         </Router>
--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -23,6 +23,18 @@ export function AuthProvider({ children }) {
     return auth.signOut();
   }
 
+  function resetPassword(email) {
+    return auth.sendPasswordResetEmail(email);
+  }
+
+  function updateEmail(email) {
+    return currentUser.updateEmail(email);
+  }
+
+  function updatePassword(password) {
+    return currentUser.updatePassword(password);
+  }
+
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged((user) => {
       setCurrentUser(user);
@@ -31,7 +43,15 @@ export function AuthProvider({ children }) {
     return unsubscribe;
   }, []);
 
-  const value = { currentUser, signup, login, logout };
+  const value = {
+    currentUser,
+    signup,
+    login,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword,
+  };
 
   return (
     <AuthContext.Provider value={value}>
