4ab4c9ca0c91a5ef27ce50dfdb635c89a696df57
Zach Auerbach <auernachzach@gmail.com>
2020-12-25 23:50:34 -0800

    added login/dashboard components as well as added react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import Signup from './Signup';
 import { Container } from 'react-bootstrap';
+import { AuthProvider } from '../contexts/AuthContext';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Dashboard from './Dashboard';
+import Login from './Login';
 
 function App() {
   return (
@@ -8,7 +12,15 @@ function App() {
       className='d-flex align-items-center justify-content-center'
       style={{ minHeight: '100vh' }}>
       <div className='w-100' style={{ maxWidth: '400px' }}>
-        <Signup />
+        <Router>
+          <AuthProvider>
+            <Switch>
+              <Route exact path='/' component={Dashboard}></Route>
+              <Route path='/signup' component={Signup}></Route>
+              <Route path='/login' component={Login}></Route>
+            </Switch>
+          </AuthProvider>
+        </Router>
       </div>
     </Container>
   );
